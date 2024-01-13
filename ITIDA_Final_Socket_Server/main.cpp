947f9d8a1331421840b91cf31e2a4524678d7de0
Ghada.Ashraf <ghadaelkassa29@gmail.com>
2024-01-14 01:09:55 +0200

    main source modified

--- ITIDA_Final_Socket_Server/main.cpp
+++ ITIDA_Final_Socket_Server/main.cpp
@@ -1,10 +1,10 @@
 #include <QCoreApplication>
 #include "server.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     Server server;
-    server.start();
-
+    server.start(2020);
     return a.exec();
 }
